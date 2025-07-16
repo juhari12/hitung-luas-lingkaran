e06ae45467c01faa17386c660390a175bd78d9ef
juhari12 <juhari@uin-malang.ac.id>
2025-07-16 09:10:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Menghitung :blue[Luas Lingkaran] :rocket:")
+
+r = st.number_input("Masukkan jari-jari:",0)
+
+if st.button("Hitung Luas Lingkaran",type="primary"):
+  L=math.pi*(r**2)
+  st.succcess(f'Luas lingkaran adalah {L:.4f})
